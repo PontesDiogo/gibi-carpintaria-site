2776e1941c689f18ebd60439527a3e66dad149de
Diogo <diogogpontes@gmail.com>
2026-05-27 10:01:26 -0300

    feat: subindo esqueleto do projeto

---
+++ src/components/pages/HomePage.tsx
@@ -0,0 +1,22 @@
+import { useEffect } from 'react'
+import { supabase } from './services/supabaseClient'
+
+function App() {
+  useEffect(() => {
+    async function loadProjects() {
+      const { data, error } = await supabase
+        .from('projects')
+        .select('*')
+        .eq('publicado', true)
+
+      console.log('Projetos:', data)
+      console.log('Erro:', error)
+    }
+
+    loadProjects()
+  }, [])
+
+  return <h1>Gibi Carpintaria</h1>
+}
+
+export default App
